19e08ab6e7cdb758200510eb5e6eefb5f81cf36f
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-03-29 01:25:52 +0330

    User hideLoader And showLoader Funcs

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 import {getMe, login, register} from "./auth.js"
+import { hideLoader, showLoader } from "./utils.js";
 getMe()
 let loginBtn = document.querySelector('#login-btn');
 let registerBtn = document.querySelector('#register-btn');
 
 loginBtn.addEventListener('click',e => {
+    showLoader();
     login()
+    hideLoader()
 })
 registerBtn.addEventListener('click',e => {
+    showLoader();
     register()
+    hideLoader()
+})
+
+window.addEventListener('load', () => {
+    hideLoader()
 })
