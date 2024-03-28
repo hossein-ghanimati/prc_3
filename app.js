01909ca6750f3d6d650bbe0cfad60fe708dd2005
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-03-29 00:33:46 +0330

    backing to <371a8c1> Commit

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
-import {getMe, register, login} from "./auth.js"
+import {getMe} from "./auth.js"
 getMe()
-let loginBtn = document.querySelector('#login-btn');
-let registerBtn = document.querySelector('#register-btn');
-
-loginBtn.addEventListener('click',e => {
-    login()
-})
-registerBtn.addEventListener('click',e => {
-    register()
-})
