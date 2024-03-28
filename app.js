c577063e516b023e656e2aec32140ffc6f0b1893
hossein-ghanimati <165092377+hossein-ghanimati@users.noreply.github.com>
2024-03-29 00:28:06 +0330

    Use login And Resgister Funcs In app.js

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-import {getMe} from "./auth.js"
+import {getMe, register, login} from "./auth.js"
 getMe()
+let loginBtn = document.querySelector('#login-btn');
+let registerBtn = document.querySelector('#register-btn');
+
+loginBtn.addEventListener('click',e => {
+    login()
+})
+registerBtn.addEventListener('click',e => {
+    register()
+})
