35a902cdad7859efab0510e3d08b819e09054b45
alien-test-acc <165730394+alien-test-acc@users.noreply.github.com>
2024-04-02 02:44:33 +0330

    Coding getArticles Func

--- utils.js
+++ utils.js
@@ -13,9 +13,17 @@ const getCourses = () => {
         {_id : 3, name : "CSS"}
     ]
 }
+const getArticles = () => {
+    return [
+        {_id : 1, name : "Canvas"},
+        {_id : 2, name : "HTML"},
+        {_id : 3, name : "CSS"}
+    ]
+}
 
 export {
     hideLoader,
     showLoader,   
-    getCourses
+    getCourses,
+    getArticles
 }
