45a76413c792b82bf56e456b39e78d9f04bc2d34
alien-test-acc <165730394+alien-test-acc@users.noreply.github.com>
2024-04-02 02:45:49 +0330

    Codein renderArticles Func

--- shared.js
+++ shared.js
@@ -1,10 +1,15 @@
-import { getCourses } from "./utils.js";
+import { getCourses, getArticles } from "./utils.js";
 
 const renderCourses = () => {
     const courses = getCourses()
     console.log("Render Courses => ", courses);
 }
+const renderArticles = () => {
+    let articles = getArticles();
+    console.log(articles)
+}
 
 export{
-    renderCourses
+    renderCourses,
+    renderArticles
 }
