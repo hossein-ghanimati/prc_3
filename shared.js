efe4d8019b276fa3890d6c9a2d96094547d86c32
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-04-02 03:10:39 +0330

    Coding renderPopularArticles Func

--- shared.js
+++ shared.js
@@ -1,15 +1,25 @@
-import { getCourses, getArticles } from "./utils.js";
+import { 
+    getCourses,
+     getArticles,
+     getPopularArticles
+     } from "./utils.js";
 
 const renderCourses = () => {
     const courses = getCourses()
     console.log("Render Courses => ", courses);
 }
 const renderArticles = () => {
-    let articles = getArticles();
-    console.log(articles)
+    const articles = getArticles();
+    console.log("Render Articles => ", articles);
+}
+
+const renderPopularArticles = () => {
+    const popularArticles = getPopularArticles()
+    console.log("Render PopularArticles => ", popularArticles);
 }
 
 export{
     renderCourses,
-    renderArticles
+    renderArticles,
+    renderPopularArticles
 }
